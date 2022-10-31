b78bc1726cbe31313cf8a59197871466a65b7174
asaad227 <a.saad227@yahoo.com>
2022-10-31 23:29:58 +0000

    filter error fixed

--- password.js
+++ password.js
@@ -1,28 +1,36 @@
 
-function passwordGenerator(e,  c, d, mix){
+function passwordGenerator(input,  capitalLetters, nums, mix){
     const password = []
-    const letters = ["A","a", "B", "C","c", "e","3","4", "1", "&", "D", "E", "3"];
+    const letters = ["A","a", "B", "!","%", "e","3","4", "1", "&", "D", "E", "3"];
     const re = /[A-Z]/g
-    const reC = /[0-9]/g
+    const reNum = /[0-9]/g
     const mixed = /[aA-zZ 0-9]/g
+    const capNum = /[A-Z 0-9]/g
     var newArr;
-    if(c){
-        newArr = letters.join("").match(reC)   
-    }
-    if(d){
+    if(capitalLetters){
         newArr = letters.join("").match(re)   
+    }
+    if(nums){
+        newArr = letters.join("").match(reNum)   
     }
     if(mix){
         newArr = letters.join("").match(mixed) 
     }
-    if(!d|| !c || !mix){
+
+    if(capitalLetters && nums){
+        newArr = letters.join("").match(capNum) 
+    }
+    if(capitalLetters && nums && mix){
+        newArr = letters.join("").match(mixed) 
+    }
+    if(!capitalLetters && !nums && !mix){
         newArr = letters;
     }
-    for(let i = 0; i < e; i++){
-        if(e > newArr.length){
+    for(let i = 0; i < input; i++){
+        if(input > newArr.length){
             password.push(newArr[Math.floor(Math.random()*newArr.length)])
         }else{
-            password.push(newArr[Math.floor(Math.random()*e)])
+            password.push(newArr[Math.floor(Math.random()*input)])
         }
        
        }
@@ -30,16 +38,20 @@ function passwordGenerator(e,  c, d, mix){
 console.log(password)
 return password.join("");
 }
+// const upper = false;
+// const num = false;
+// const mix = false;
+// console.log(passwordGenerator(12, upper, num, mix))
 const btn = document.querySelector(".password")
 
 function generate(){
-    const mixed = document.querySelector(".mixed")
-    const upper = document.querySelector(".cap");
-    const num = document.querySelector(".nums");
-    const inputValue = document.querySelector(".inputNum");
-    console.log(upper.checked, num.checked, inputValue.value, mixed.checked)
+    const mixed = document.querySelector(".mixed").checked
+    const upper = document.querySelector(".cap").checked;
+    const num = document.querySelector(".nums").checked;
+    const inputValue = document.querySelector(".inputNum").value;
+    console.log(inputValue, upper, num, mixed)
 
-    const password = passwordGenerator(inputValue.value, upper.checked, mixed.checked, num.checked);
+    const password = passwordGenerator(inputValue, upper, num, mixed);
     console.log(password)
     document.querySelector(".result").innerHTML = `Password: ${password}`
 }
