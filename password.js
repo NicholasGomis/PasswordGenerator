6509e6e9497bb3ec7a6ef1d4bc68a6cf4f590fd1
asaad227 <a.saad227@yahoo.com>
2022-10-31 23:36:10 +0000

    letter checked box created as checked

--- password.js
+++ password.js
@@ -1,5 +1,5 @@
 
-function passwordGenerator(input,  capitalLetters, nums, mix){
+function passwordGenerator(input, characters,  capitalLetters, nums, mix){
     const password = []
     const letters = ["A","a", "B", "!","%", "e","3","4", "1", "&", "D", "E", "3"];
     const re = /[A-Z]/g
@@ -23,7 +23,7 @@ function passwordGenerator(input,  capitalLetters, nums, mix){
     if(capitalLetters && nums && mix){
         newArr = letters.join("").match(mixed) 
     }
-    if(!capitalLetters && !nums && !mix){
+    if(characters && !capitalLetters && !nums && !mix){
         newArr = letters;
     }
     for(let i = 0; i < input; i++){
@@ -45,13 +45,14 @@ return password.join("");
 const btn = document.querySelector(".password")
 
 function generate(){
-    const mixed = document.querySelector(".mixed").checked
+    const mixed = document.querySelector(".mixed").checked;
+    const characters = document.querySelector(".letters").checked
     const upper = document.querySelector(".cap").checked;
     const num = document.querySelector(".nums").checked;
     const inputValue = document.querySelector(".inputNum").value;
-    console.log(inputValue, upper, num, mixed)
+    console.log(inputValue, characters, upper, num, mixed)
 
-    const password = passwordGenerator(inputValue, upper, num, mixed);
+    const password = passwordGenerator(inputValue, characters, upper, num, mixed);
     console.log(password)
     document.querySelector(".result").innerHTML = `Password: ${password}`
 }
