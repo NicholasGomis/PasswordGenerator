1b8c35189d9fd9ddbb9f32637c567e1f82a7e683
asaad227 <a.saad227@yahoo.com>
2022-11-01 18:32:28 +0000

    refactor the function and also update dom

--- password.js
+++ password.js
@@ -1,69 +1,48 @@
-let containsSymbols = true;
-
-<<<<<<< HEAD
-function password(e, input){
-    const letters = ["A", "B", "C", "1", "%","&", "D", "E", "3", "!", "$"];
-    let newArr;
-    const regx = /[Aa-Zz]/gi
-    const password = []
-    
-    if (input){
-    newArr =  letters.join('').match(regx)
-    
-    } else {
-        
-
-        newArr = letters
-    }
-
-    for(let i = 0; i < e; i++){
-        password.push(letters[Math.floor(Math.random()*e)])
-=======
 function passwordGenerator(input, characters,  capitalLetters, nums, mix){
-    const password = []
-    const letters = ["A","a", "B", "!","%", "e","3","4", "1", "&", "D", "E", "3"];
+    const alpha = Array.from(Array(94)).map((e, i)=> i+33);
+    const newArr = alpha.map((e)=> String.fromCharCode(e));
+    var password;
     const re = /[A-Z]/g
     const reNum = /[0-9]/g
     const mixed = /[aA-zZ 0-9]/g
     const capNum = /[A-Z 0-9]/g
-    var newArr;
-    if(capitalLetters){
-        newArr = letters.join("").match(re)   
-    }
-    if(nums){
-        newArr = letters.join("").match(reNum)   
-    }
-    if(mix){
-        newArr = letters.join("").match(mixed) 
->>>>>>> 6509e6e9497bb3ec7a6ef1d4bc68a6cf4f590fd1
-    }
+   if(capitalLetters){
+    const cap = newArr.join("").match(re);
+    password = cap.sort(()=> Math.random() - Math.random()).slice(0, input)
+   }
+   if(nums){
+    const cap = newArr.join("").match(reNum);
+    password = cap.sort(()=> Math.random() - Math.random()).slice(0, input)
+   }
+   if(mix){
+    const cap = newArr.join("").match(mixed);
+    password = cap.sort(()=> Math.random() - Math.random()).slice(0, input)
+   }
 
-    if(capitalLetters && nums){
-        newArr = letters.join("").match(capNum) 
-    }
-    if(capitalLetters && nums && mix){
-        newArr = letters.join("").match(mixed) 
-    }
-    if(characters && !capitalLetters && !nums && !mix){
-        newArr = letters;
-    }
-    for(let i = 0; i < input; i++){
-        if(input > newArr.length){
-            password.push(newArr[Math.floor(Math.random()*newArr.length)])
-        }else{
-            password.push(newArr[Math.floor(Math.random()*input)])
-        }
-       
-       }
+   if(nums && capitalLetters){
+    const cap = newArr.join("").match(capNum);
+    password = cap.sort(()=> Math.random() - Math.random()).slice(0, input)
+   }
 
+   if(characters){
+    password = newArr.sort(()=> Math.random() - Math.random()).slice(0, input)
+   }
+ 
 console.log(password)
 return password.join("");
 }
 // const upper = false;
 // const num = false;
-// const mix = false;
-// console.log(passwordGenerator(12, upper, num, mix))
+// const mix = true;
+// const letters = false;
+// console.log(passwordGenerator(12, letters, upper, num, mix))
 const btn = document.querySelector(".password")
+
+function inputV(){
+    const inputValue = document.querySelector(".inputNum").value;
+    document.querySelector(".inputV").innerHTML = `Characters Numbers: ${inputValue}`
+}
+
 
 function generate(){
     const mixed = document.querySelector(".mixed").checked;
@@ -72,15 +51,11 @@ function generate(){
     const num = document.querySelector(".nums").checked;
     const inputValue = document.querySelector(".inputNum").value;
     console.log(inputValue, characters, upper, num, mixed)
-
+    document.querySelector(".inputV").innerHTML = `Characters Numbers: ${inputValue}`
     const password = passwordGenerator(inputValue, characters, upper, num, mixed);
     console.log(password)
     document.querySelector(".result").innerHTML = `Password: ${password}`
 }
 
 
-<<<<<<< HEAD
-console.log(password(6,containsSymbols))
-=======
 btn.addEventListener("click",  generate)
->>>>>>> 6509e6e9497bb3ec7a6ef1d4bc68a6cf4f590fd1
