cf88fe4f62605fd1e826ffea0700ff0ff94fc32e
NicholasGomis <nicholasgomis@icloud.com>
2022-11-01 17:20:00 +0000

    Added different logic in passwordtwo.js

--- password.js
+++ password.js
@@ -1,8 +1,20 @@
+let containsSymbols = true;
 
-function password(e){
-    const letters = ["A", "B", "C", "1", "&", "D", "E", "3"];
-
+function password(e, input){
+    const letters = ["A", "B", "C", "1", "%","&", "D", "E", "3", "!", "$"];
+    let newArr;
+    const regx = /[Aa-Zz]/gi
     const password = []
+    
+    if (input){
+    newArr =  letters.join('').match(regx)
+    
+    } else {
+        
+
+        newArr = letters
+    }
+
     for(let i = 0; i < e; i++){
         password.push(letters[Math.floor(Math.random()*e)])
     }
@@ -10,4 +22,4 @@ function password(e){
     return password.join("")
 }
 
-console.log(password(6))
+console.log(password(6,containsSymbols))
